659a677caf3534d877eeb49c7a87259e5e1f5e7d
NuzzoFrancesco02 <francesco1.nuzzo@mail.polimi.it>
2024-05-23 17:13:44 +0200

    last commit, I hope

--- pdf2excel.py
+++ pdf2excel.py
@@ -1,4 +1,3 @@
-import pdfplumber
 import numpy as np
 import re
 from openpyxl import Workbook, load_workbook
@@ -17,6 +16,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
 from pdfminer.high_level import extract_text
+import requests
+from bs4 import BeautifulSoup
+
+
+def cerca_link_google(title):
+    title = re.sub(' ','+',title)
+    url = 'https://scholar.google.com/scholar?hl=it&as_sdt=0%2C5&q='+title+'&btnG='
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        links = soup.find_all("a", href=True)
+        for link in links:
+            href = link.get("href")
+            print(href)
+
+cerca_link_google('A method for predicting multivariate random loads and a discrete appoximation of the mutidimensional design load envelope')
+
 
 proxies = ['http://38.242.158.32:443',
     'http://185.105.91.159:4444',
@@ -36,69 +53,45 @@ proxies = ['http://38.242.158.32:443',
 ]
 def random_delay():
     time.sleep(random.uniform(1, 5))
-
-def get_random_proxy():
-    """Returns a random proxy from the list."""
-    return random.choice(proxies)
-
 def cerca(book_title):
-    """Searches for a book on Google Scholar and returns the link to the first result."""
-    # Setup Chrome options for headless browsing
-    proxy = get_random_proxy()
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    #options.add_argument("--proxy-server=socks5://127.0.0.1:9050")
-    options.add_argument(
-        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    )
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
-    # Initialize the WebDriver
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     try:
-        # Open Google Scholar
         driver.get("https://scholar.google.com/")
         random_delay()
 
-        # Locate the search box and enter the book title
         search_box = driver.find_element(By.NAME, "q")
         search_box.send_keys(book_title)
         random_delay()
         search_box.send_keys(Keys.RETURN)
 
         random_delay()
-
-        # Get the search results
         results = driver.find_elements(By.CLASS_NAME, "gs_ri")
-        #print(f"Found {len(results)} results")  # Debug: print number of results found
 
-        # Check if there are any results
-        if not results:
-            print("No results found")
-            return None
-
-        # Extract the link from the first result
         for idx, result in enumerate(results, 1):
-            try:
-                title_element = result.find_element(By.TAG_NAME, "h3")
-                link = title_element.find_element(By.TAG_NAME, 'a').get_attribute('href')
-                snippet = result.find_element(By.CLASS_NAME, 'gs_rs').text
+            title_element = result.find_element(By.TAG_NAME, "h3")
+            title = title_element.text
+            link = title_element.find_element(By.TAG_NAME, 'a').get_attribute('href')
+            snippet = result.find_element(By.CLASS_NAME, 'gs_rs').text
 
-                # Return the link of the first result
-                if idx > 1:
-                    return link
-            except NoSuchElementException as e:
-                print(f"Error in result {idx}: {e}")
-
-    except NoSuchElementException as e:
-        print(f"Error during search: {e}")
-        return None
-
+            if idx > 1:
+                break
+            
+            #print(f"Link: {link}")
+            return link
+           
+    except NoSuchElementException:
+        return 'checked'
+        
     finally:
-        # Ensure the driver quits properly
         driver.quit()
+
 def clean_text(paragrafo,IFASD_name):
     if '\n' in paragrafo:
         paragrafo = re.sub(r'(?<=[a-zA-Z])\n|(?=\n[a-zA-Z])',' ',paragrafo)
@@ -112,157 +105,4 @@ def clean_text(paragrafo,IFASD_name):
     if '- ' in paragrafo:
         paragrafo = re.sub('- ','',paragrafo)
     IFASD_name=re.sub('.pdf','',IFASD_name)
-    pat = r'1\s+' + re.escape(IFASD_name)
-    if re.search(pat, paragrafo):
-        paragrafo = re.sub(pat,'',paragrafo)
-    return(paragrafo)
-#print(cerca('Cross-modal Damping Model: an experimental extraction approach and aircraft dynamic loads application'))
-wb = load_workbook('FINAL PAPER IFASD 2017/Repository Upload Information_IFASD_2017.xlsx')
-
-ws = wb.active
-titles = ''
-for row in np.arange(9,172,1):
-    flag_abstract = flag_keywords = flag_online = True
-    if ws['G'+str(row)].value == None:
-        flag_abstract = False
-    if ws['I'+str(row)].value == None:
-        flag_keywords = False
-    if ws['J'+str(row)].value == None:
-        flag_online = False
-    
-    
-
-    
-    if not flag_online or not flag_keywords or not flag_abstract:
-        is_already_open = False
-        IFASD_name = ws['F'+str(row)].value
-        #extract_text(
-
-        #with pdfplumber.open(pdf_path) as pdf:
-        if not flag_abstract:
-            if not is_already_open:
-                
-                #pdf_path = os.path.join('FINAL PAPER IFASD 2017',IFASD_name)
-                texts = extract_text('FINAL PAPER IFASD 2017/'+IFASD_name,page_numbers=[0,1])
-                #first_page = pdf.pages[0]
-                
-                #second_page = pdf.pages[1]
-                #pattern = r'\bK\s*e\s*y\s*w\s*o\s*r\s*d\s*s\b'
-                '''
-                if not re.search(pattern, paragrafo, re.IGNORECASE):
-                    first_page = pdf.pages[1]
-                    paragrafo = first_page.extract_text_simple(x_tolerance=1, y_tolerance=5)
-                '''
-                is_already_open = True
-            #paragrafo1 = first_page.extract_text_simple(x_tolerance=1, y_tolerance=5)
-            #paragrafo2 = second_page.extract_text_simple(x_tolerance=1, y_tolerance=5)
-            #paragrafo = paragrafo1 + ' ' + paragrafo2
-            paragrafo = texts
-            paragrafo = clean_text(paragrafo,IFASD_name)
-            pattern = r'Abstract:\s*(.*?)[\s*\n]?[\d]\s*INTRODUCTION'
-            #pattern = r'Abstract:\s*(.*?)\s*1\s*INTRODUCTION'
-            
-            try:
-                match = re.search(pattern, paragrafo,re.DOTALL)
-                paragrafo = match.group(1)  
-            except:
-                try: 
-                    pattern = r'Abstract:\s*(.*?)[\s*\n]?\s*List of Symbols'
-                    match = re.search(pattern, paragrafo,re.DOTALL)
-                    paragrafo = match.group(1)         
-                except:
-                    try:
-                        pattern = r'Abstract:\s*(.*?)[\s*\n]?\s*Introduction'
-                        match = re.search(pattern, paragrafo,re.DOTALL)
-                        paragrafo = match.group(1) 
-                    except:       
-                        try:
-                            pattern = r'Abstract:\s*(.*?)[\s*\n]?\s*Notice to Readers'
-                            match = re.search(pattern, paragrafo,re.DOTALL)
-                            paragrafo = match.group(1) 
-                        except:
-                            try:
-                                pattern = r'Abstract:?\s*(.*?)\s*\n*\s*INTRODUCTION'
-                                match = re.search(pattern, paragrafo,re.DOTALL)
-                                paragrafo = match.group(1) 
-                            except:
-                                try:
-                                    pattern = r'Abstract:?\s*(.*?)\s*\n*\s*INTRODUCYION'
-                                    match = re.search(pattern, paragrafo,re.DOTALL)
-                                    paragrafo = match.group(1) 
-                                except:
-                                    try:
-                                        pattern = r'Abstract:?\s*(.*?)\s*\n*\s*NOMENCLATURE'
-                                        match = re.search(pattern, paragrafo,re.DOTALL)
-                                        paragrafo = match.group(1) 
-                                    except:
-                                        pattern = r'Abstract:?\s*(.*?)\s*\n*\s*NOTATION'
-                                        match = re.search(pattern, paragrafo,re.DOTALL)
-                                        paragrafo = match.group(1) 
-                                
-
-            
-            
-            #print(paragrafo + '\n\n')
-            ws['G'+str(row)].value = paragrafo
-        
-        if not flag_keywords and IFASD_name!='IFASD-2017-180.pdf' and IFASD_name!='IFASD-2017-181.pdf':
-            if not is_already_open:
-                IFASD_name = ws['F'+str(row)].value
-                #pdf_path = os.path.join('FINAL PAPER IFASD 2017',IFASD_name)
-                texts = extract_text('FINAL PAPER IFASD 2017/'+IFASD_name,page_numbers=[0,1])
-                #first_page = pdf.pages[0]                   
-                #second_page = pdf.pages[1]
-                is_already_open = True
-            #paragrafo1 = first_page.extract_text_simple(x_tolerance=1, y_tolerance=1)
-            #paragrafo2 = second_page.extract_text_simple(x_tolerance=1, y_tolerance=1)
-            #paragrafo = paragrafo1 + ' ' + paragrafo2
-            paragrafo = texts
-            pattern =  r'Keywords:\s*(.*?)\s*[\n\s]*Abstract\s*:'
-            #paragrafo = first_page.extract_text_simple(x_tolerance=5, y_tolerance=5)
-            paragrafo = clean_text(paragrafo,IFASD_name)
-            try:
-                match = re.search(pattern, paragrafo,re.DOTALL)   
-                paragrafo = match.group(1)
-            except:
-                try:
-                    pattern =  r'Kewywords:\s*(.*?)[\s*\n]?\s*INTRODUCTION'
-                    match = re.search(pattern, paragrafo,re.DOTALL)   
-                    paragrafo = match.group(1)
-                except:
-                    try:
-                        pattern =  r'Kewywords:\s*(.*?)[\s*\n]?\s*Introduction'
-                        match = re.search(pattern, paragrafo,re.DOTALL)   
-                        paragrafo = match.group(1)
-                    except:
-                        pattern =  r'Keywords:\s*(.*?)\s*1\s+INTRODUCTION'
-                        match = re.search(pattern, paragrafo,re.DOTALL)   
-                        paragrafo = match.group(1)
-            
-            
-            #print(paragrafo + '\n\n')
-            ws['I'+str(row)].value = paragrafo
-        if not flag_online or flag_online:
-            
-            title = ws['B'+str(row)].value
-            if '\n' in title:          
-                title = re.sub(r'\n+',' ',title)
-                
-            if '  ' in title:
-                pat = r'\s+'
-                title = re.sub(pat,' ',title)
-            
-            #title = 'Comparison of High-Fidelity Aero-Structure Gradient Computation Techniques. Application on the CRM Wing Design.'
-            print('\n'+title)
-            #title = re.sub(' ','+',title)
-            #site = "https://scholar.google.com/scholar?hl=it&as_sdt=0%2C5&q="+title+".&btnG="
-            #link = cerca(title)
-            #if link == None:
-            #    link = 'checked'
-            #print(link)
-            print('\n')
-            titles = titles + title +'\n'
-            #ws['J'+str(row)].value = link
-with open('titles.txt', "w") as file_titles:
-    file_titles.write(titles)             
-wb.save('FINAL PAPER IFASD 2017/Repository Upload Information_IFASD_2017.xlsx')
+    pat = r'1\s+' + re.esca
