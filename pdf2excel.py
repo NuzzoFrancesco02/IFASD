2fd99ed6ebecef1adf58eda7e88d48325228ce45
NuzzoFrancesco02 <francesco1.nuzzo@mail.polimi.it>
2024-07-12 21:17:42 +0200

    prova

--- pdf2excel.py
+++ pdf2excel.py
@@ -105,4 +105,122 @@ def clean_text(paragrafo,IFASD_name):
     if '- ' in paragrafo:
         paragrafo = re.sub('- ','',paragrafo)
     IFASD_name=re.sub('.pdf','',IFASD_name)
-    pat = r'1\s+' + re.esca
+    pat = r'1\s+' + re.escape(IFASD_name)
+    if re.search(pat, paragrafo):
+        paragrafo = re.sub(pat,'',paragrafo)
+    return(paragrafo)
+wb = load_workbook('FINAL PAPER IFASD 2017/Repository Upload Information_IFASD_2017.xlsx')
+
+ws = wb.active
+titles = ''
+for row in np.arange(9,172,1):
+    flag_abstract = flag_keywords = flag_online = True
+    if ws['G'+str(row)].value == None:
+        flag_abstract = False
+    if ws['I'+str(row)].value == None:
+        flag_keywords = False
+    if ws['J'+str(row)].value == None:
+        flag_online = False
+    
+    
+
+    
+    if not flag_online or not flag_keywords or not flag_abstract:
+        is_already_open = False
+        IFASD_name = ws['F'+str(row)].value
+        if not flag_abstract:
+            if not is_already_open:
+                texts = extract_text('FINAL PAPER IFASD 2017/'+IFASD_name,page_numbers=[0,1])
+                is_already_open = True
+            paragrafo = texts
+            paragrafo = clean_text(paragrafo,IFASD_name)
+            pattern = r'Abstract:\s*(.*?)[\s*\n]?[\d]\s*INTRODUCTION'
+            try:
+                match = re.search(pattern, paragrafo,re.DOTALL)
+                paragrafo = match.group(1)  
+            except:
+                try: 
+                    pattern = r'Abstract:\s*(.*?)[\s*\n]?\s*List of Symbols'
+                    match = re.search(pattern, paragrafo,re.DOTALL)
+                    paragrafo = match.group(1)         
+                except:
+                    try:
+                        pattern = r'Abstract:\s*(.*?)[\s*\n]?\s*Introduction'
+                        match = re.search(pattern, paragrafo,re.DOTALL)
+                        paragrafo = match.group(1) 
+                    except:       
+                        try:
+                            pattern = r'Abstract:\s*(.*?)[\s*\n]?\s*Notice to Readers'
+                            match = re.search(pattern, paragrafo,re.DOTALL)
+                            paragrafo = match.group(1) 
+                        except:
+                            try:
+                                pattern = r'Abstract:?\s*(.*?)\s*\n*\s*INTRODUCTION'
+                                match = re.search(pattern, paragrafo,re.DOTALL)
+                                paragrafo = match.group(1) 
+                            except:
+                                try:
+                                    pattern = r'Abstract:?\s*(.*?)\s*\n*\s*INTRODUCYION'
+                                    match = re.search(pattern, paragrafo,re.DOTALL)
+                                    paragrafo = match.group(1) 
+                                except:
+                                    try:
+                                        pattern = r'Abstract:?\s*(.*?)\s*\n*\s*NOMENCLATURE'
+                                        match = re.search(pattern, paragrafo,re.DOTALL)
+                                        paragrafo = match.group(1) 
+                                    except:
+                                        pattern = r'Abstract:?\s*(.*?)\s*\n*\s*NOTATION'
+                                        match = re.search(pattern, paragrafo,re.DOTALL)
+                                        paragrafo = match.group(1) 
+                if paragrafo[-1].isdigit():
+                    paragrafo = paragrafo[:-1]
+            ws['G'+str(row)].value = paragrafo     
+        if not flag_keywords and IFASD_name!='IFASD-2017-180.pdf' and IFASD_name!='IFASD-2017-181.pdf':
+            if not is_already_open:
+                IFASD_name = ws['F'+str(row)].value
+                texts = extract_text('FINAL PAPER IFASD 2017/'+IFASD_name,page_numbers=[0,1])
+                is_already_open = True
+            paragrafo = texts
+            pattern =  r'Keywords:\s*(.*?)\s*[\n\s]*Abstract\s*:'
+            paragrafo = clean_text(paragrafo,IFASD_name)
+            try:
+                match = re.search(pattern, paragrafo,re.DOTALL)   
+                paragrafo = match.group(1)
+            except:
+                try:
+                    pattern =  r'Kewywords:\s*(.*?)[\s*\n]?\s*INTRODUCTION'
+                    match = re.search(pattern, paragrafo,re.DOTALL)   
+                    paragrafo = match.group(1)
+                except:
+                    try:
+                        pattern =  r'Kewywords:\s*(.*?)[\s*\n]?\s*Introduction'
+                        match = re.search(pattern, paragrafo,re.DOTALL)   
+                        paragrafo = match.group(1)
+                    except:
+                        try:
+                            pattern =  r'Keywords:\s*(.*?)\s*[\n\s]*Abstract '
+                            match = re.search(pattern, paragrafo,re.DOTALL)   
+                            paragrafo = match.group(1)
+                        except:
+                            pattern =  r'Keywords:\s*(.*?)\s*1\s+INTRODUCTION'
+                            match = re.search(pattern, paragrafo,re.DOTALL)   
+                            paragrafo = match.group(1)
+                if paragrafo[-1].isdigit():
+                    paragrafo = paragrafo[:-1]
+            ws['I'+str(row)].value = paragrafo
+        if not flag_online or flag_online:
+            
+            title = ws['B'+str(row)].value
+            if '\n' in title:          
+                title = re.sub(r'\n+',' ',title)
+                
+            if '  ' in title:
+                pat = r'\s+'
+                title = re.sub(pat,' ',title)
+            print('\n'+title)
+            print('\n')
+            titles = titles + title +'\n'
+            #ws['J'+str(row)].value = link
+with open('titles.txt', "w") as file_titles:
+    file_titles.write(titles)             
+wb.save('FINAL PAPER IFASD 2017/Repository Upload Information_IFASD_2017.xlsx')
