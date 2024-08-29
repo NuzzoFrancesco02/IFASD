23ddc0f9ab2934f031c7d644c700cd45d708e016
NuzzoFrancesco02 <148267807+NuzzoFrancesco02@users.noreply.github.com>
2024-08-29 19:47:34 +0200

    Finished

--- pdf2excel.py
+++ pdf2excel.py
@@ -25,15 +25,19 @@ def clean_text(paragrafo,IFASD_name):
     if re.search(pat, paragrafo):
         paragrafo = re.sub(pat,'',paragrafo)
     return(paragrafo)
-wb = load_workbook('/Users/francesconuzzo/Downloads/PROCEEDINGS/Cartel1.xlsx')
+wb = load_workbook('/Users/francesconuzzo/Downloads/USB_Proceedings/info/papers/Cartel1.xlsx')
 
 ws = wb.active
 titles = ''
-raw_begin = 130
-raw_end = 138
-indice = raw_begin-8
+raw_begin = 147
+raw_end = 210
+
+if isinstance(ws['E'+str(raw_begin - 1)].value, int):
+    indice = ws['E'+str(raw_begin - 1)].value + 1
+else:
+    indice = raw_begin-8
 for row in np.arange(raw_begin,raw_end,1):
-    if indice == 131:
+    if indice == 209:
         break
     flag_authors = flag_conf_numb = flag_abstract = flag_file_name = flag_keywords = flag_category = flag_online = True
     flag_title = False
@@ -60,23 +64,23 @@ for row in np.arange(raw_begin,raw_end,1):
     if not flag_title or not flag_authors or not flag_conf_numb or not flag_file_name or not flag_category or not flag_online or not flag_keywords or not flag_abstract:
         is_already_open = False
         is_file_flag = False
-        while not is_file_flag and indice <= 156:
+        while not is_file_flag and indice <= 208:
             if indice < 10:
-                IFASD_name = 'IFASD-2019-00'+str(indice)+'.pdf'
+                IFASD_name = 'IFASD-2015-00'+str(indice)+'.pdf'
                 if os.path.isfile('/Users/francesconuzzo/Downloads/PROCEEDINGS/'+IFASD_name):
                     
                     is_file_flag = True
                 else:
                     indice = indice + 1
             elif indice < 100:
-                IFASD_name = 'IFASD-2019-0' + str(indice)+'.pdf'
+                IFASD_name = 'IFASD-2015-0' + str(indice)+'.pdf'
                 if os.path.isfile('/Users/francesconuzzo/Downloads/PROCEEDINGS/'+IFASD_name):
                 
                     is_file_flag = True
                 else:
                     indice = indice + 1
             else:
-                IFASD_name = 'IFASD-2019-' + str(indice)+'.pdf'
+                IFASD_name = 'IFASD-2015-' + str(indice)+'.pdf'
                 if os.path.isfile('/Users/francesconuzzo/Downloads/PROCEEDINGS/'+IFASD_name):
                     
                     is_file_flag = True
@@ -91,7 +95,7 @@ for row in np.arange(raw_begin,raw_end,1):
         #if not 'IFASD-2015-054.pdf' == IFASD_name and not 'IFASD-2015-181.pdf' == IFASD_name and not 'IFASD-2015-206.pdf' == IFASD_name and not 'IFASD-2015-207.pdf' == IFASD_name:
             if not flag_title:
                 if not is_already_open:
-                    texts = extract_text('/Users/francesconuzzo/Downloads/PROCEEDINGS/'+IFASD_name,page_numbers=[0,1])
+                    texts = extract_text('/Users/francesconuzzo/Downloads/USB_Proceedings/info/papers/'+IFASD_name,page_numbers=[0,1])
                     is_already_open = True
                     paragrafo = texts
                     
@@ -103,14 +107,14 @@ for row in np.arange(raw_begin,raw_end,1):
                 
                 if up_title_check.isupper():
                     #pattern = r"(^[^a-z]*)([a-z].*)"
-                    pattern =r'USA\s*(.*?)(?:\r?\n\r?\n)'
+                    pattern =r'\s*(.*?)(?:\r?\n\r?\n)'
                     match = re.search(pattern, paragrafo,re.DOTALL)
                     paper_title = match.group(1)
                     paper_title = paper_title[:-2]
                     beg, title_pos = match.regs[1]
                 else:
                     #pattern =r'(.*?)(?:\r?\n\r?\n)'
-                    pattern =r'USA\s*(.*?)(?:\r?\n\r?\n)'
+                    pattern =r'\s*(.*?)(?:\r?\n\r?\n)'
                     match = re.findall(pattern,paragrafo,re.DOTALL)
                     paper_title = match[0]
                     title_pos = len(paper_title)
@@ -118,11 +122,12 @@ for row in np.arange(raw_begin,raw_end,1):
                 ws['B'+str(row)].value = clean_text(paper_title,IFASD_name)
             if not flag_authors:
                 if not is_already_open:
-                    texts = extract_text('/Users/francesconuzzo/Downloads/PROCEEDINGS/'+IFASD_name,page_numbers=[0,1])
+                    texts = extract_text('/Users/francesconuzzo/Downloads/USB_Proceedings/info/papers/'+IFASD_name,page_numbers=[0,1])
                     is_already_open = True
                     paragrafo = texts
                     #paragrafo = clean_text(paragrafo,IFASD_name)
-                pattern = re.escape(paper_title) + r'\n*([^\n]+)'
+                #pattern = re.escape(paper_title) + r'\n*([^\n]+)'
+                pattern = re.escape(paper_title) + r'\n*(.+?)(?=\n{2,})'
                 match = re.findall(pattern, paragrafo, re.DOTALL)
 
                 if match:
@@ -137,7 +142,7 @@ for row in np.arange(raw_begin,raw_end,1):
                 ws['F'+str(row)].value = IFASD_name
             if not flag_abstract:
                 if not is_already_open:
-                    texts = extract_text('/Users/francesconuzzo/Downloads/PROCEEDINGS/'+IFASD_name,page_numbers=[0,1])
+                    texts = extract_text('/Users/francesconuzzo/Downloads/USB_Proceedings/info/papers/'+IFASD_name,page_numbers=[0,1])
                     is_already_open = True
                 paragrafo = texts
                 paragrafo = clean_text(paragrafo,IFASD_name)
@@ -217,7 +222,7 @@ for row in np.arange(raw_begin,raw_end,1):
             #if not flag_keywords and IFASD_name!='IFASD-2017-180.pdf' and IFASD_name!='IFASD-2017-181.pdf':
                 if not is_already_open:
                     IFASD_name = ws['F'+str(row)].value
-                    texts = extract_text('/Users/francesconuzzo/Downloads/PROCEEDINGS/'+IFASD_name,page_numbers=[0,1])
+                    texts = extract_text('/Users/francesconuzzo/Downloads/USB_Proceedings/info/papers/'+IFASD_name,page_numbers=[0,1])
                     is_already_open = True
                 paragrafo = texts
                 pattern =  r'Keywords:\s*(.*?)\s*[\n\s]*Abstract\s*:'
@@ -283,6 +288,8 @@ for row in np.arange(raw_begin,raw_end,1):
 
                 titles = titles + title +'\n'
                 #ws['J'+str(row)].value = link
+        else:
+            row = row + 1
 with open('titles.txt', "w") as file_titles:
     file_titles.write(titles)             
-wb.save('/Users/francesconuzzo/Downloads/PROCEEDINGS/Cartel1.xlsx')
+wb.save('/Users/francesconuzzo/Downloads/USB_Proceedings/info/papers/Cartel1.xlsx')
